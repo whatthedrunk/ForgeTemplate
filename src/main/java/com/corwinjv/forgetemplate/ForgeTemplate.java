d08ab39b8bd7b02f537bc79b350cd872b7437387
CorwinJV <corwin.j@gmail.com>
2016-12-11 23:30:10 -0800

    Add ConfigurationHandler init in preInit

--- src/main/java/com/corwinjv/forgetemplate/ForgeTemplate.java
+++ src/main/java/com/corwinjv/forgetemplate/ForgeTemplate.java
@@ -4,7 +4,9 @@ package com.corwinjv.forgetemplate;
  */
 
 import com.corwinjv.forgetemplate.blocks.ModBlocks;
+import com.corwinjv.forgetemplate.config.ConfigurationHandler;
 import com.corwinjv.forgetemplate.proxy.CommonProxy;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -24,6 +26,10 @@ public class ForgeTemplate
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
+        // Config
+        ConfigurationHandler.Init(event.getSuggestedConfigurationFile());
+        MinecraftForge.EVENT_BUS.register(new ConfigurationHandler());
+
         // Blocks and Items
         ModBlocks.init();
         ModBlocks.registerBlocks();
